908d33fd6f0f94f68496139f19f7337bf946ae1d
Markus Fischer <fisch3r@gmail.com>
2013-01-21 23:25:05 +0100

    added flyway to the project

---
+++ src/main/resources/db/testdata/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO  developers (name, username, email, passowrd) VALUES ('Nils', 'neckert', 'mail@nils-eckert.de', SHA1( 'neckert'));
+INSERT INTO  developers (name, username, email, passowrd) VALUES ('Benjamin', 'basbach', 'mail@nils-eckert.de', SHA1( 'basbach'));
+INSERT INTO  developers (name, username, email, passowrd) VALUES ('Markus', 'mfischer', 'mail@nils-eckert.de', SHA1( 'mfischer'));
+INSERT INTO  developers (name, username, email, passowrd) VALUES ('Michel', 'mzedler', 'mail@nils-eckert.de', SHA1( 'mzedler'));
+INSERT INTO  developers (name, username, email, passowrd) VALUES ('Jens', 'jmertz', 'mail@nils-eckert.de', SHA1( 'jmertz'));
+INSERT INTO  developers (name, username, email, passowrd) VALUES ('Phil', 'pmeier', 'mail@nils-eckert.de', SHA1( 'pmeier'));
+INSERT INTO  developers (name, username, email, passowrd) VALUES ('Timo', 'tguenzel', 'mail@nils-eckert.de', SHA1( 'tguenzel'));
+
+INSERT INTO version (version) VALUES (6);
