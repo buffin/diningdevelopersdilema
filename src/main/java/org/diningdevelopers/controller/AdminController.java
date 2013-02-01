119f589b3b2ce9707b6988bf82857b374f5e222e
Nils Eckert <nils.eckert@eckert-partner.it>
2013-02-01 08:31:22 +0100

    Admin Button to send a Test-Message to the currently logged in user

--- src/main/java/org/diningdevelopers/controller/AdminController.java
+++ src/main/java/org/diningdevelopers/controller/AdminController.java
@@ -1,6 +1,7 @@
 package org.diningdevelopers.controller;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
@@ -9,8 +10,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.diningdevelopers.dao.JpaUtils;
+import org.diningdevelopers.entity.Developer;
+import org.diningdevelopers.model.SimpleMail;
 import org.diningdevelopers.service.DecisionService;
+import org.diningdevelopers.service.DeveloperService;
+import org.diningdevelopers.service.MailService;
 import org.diningdevelopers.service.VoteService;
+import org.diningdevelopers.utils.Authentication;
 import org.diningdevelopers.utils.FacesUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +33,11 @@ public class AdminController implements Serializable {
 
 	@Inject
 	private DecisionService decisionService;
+
+	@Inject
+	private MailService mailService;
+
+	@Inject DeveloperService developerService;
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -60,6 +71,21 @@ public class AdminController implements Serializable {
 
 	public String closeVoting() {
 		voteService.closeVoting();
+		return null;
+	}
+
+	public String sendTestMail() {
+		SimpleMail mail = new SimpleMail();
+
+		String username = Authentication.getUsername();
+		Developer developer = developerService.findByUsername(username);
+
+		mail.setTo(Arrays.asList(developer.getEmail()));
+		mail.setSubject("DDD Testmail");
+		mail.setBody("Theaterhaus rockt!");
+
+		mailService.sendMail(mail);
+
 		return null;
 	}
 }
