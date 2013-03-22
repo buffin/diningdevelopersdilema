f446c87a336d4e1361380bb7426b6e56b6a1fe56
Nils Eckert <nils.eckert@eckert-partner.it>
2013-03-22 20:12:29 +0100

    Create FacesHelper to improve mockability

---
+++ src/main/java/org/diningdevelopers/utils/FacesHelper.java
@@ -0,0 +1,39 @@
+package org.diningdevelopers.utils;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Conversation;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@ApplicationScoped
+public class FacesHelper {
+
+	public void addMessage(String message) {
+		FacesMessage facesMessage = new FacesMessage(message);
+		getContext().addMessage(null, facesMessage);
+	}
+
+	public FacesContext getContext() {
+		return FacesContext.getCurrentInstance();
+	}
+
+	public boolean isNotPostback() {
+		return isPostback() == false;
+	}
+
+	public boolean isPostback() {
+		return getContext().isPostback();
+	}
+
+	public void beginConversation(Conversation conversation) {
+		if (conversation.isTransient()) {
+			conversation.begin();
+		}
+	}
+
+	public void endConversation(Conversation conversation) {
+		if (conversation.isTransient() == false) {
+			conversation.end();
+		}
+	}
+}
