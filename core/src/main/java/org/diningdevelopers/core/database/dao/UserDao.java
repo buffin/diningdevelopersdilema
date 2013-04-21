35f788beb3869c5073bfc35b3c07aa83110af167
Markus Fischer <fisch3r@gmail.com>
2013-04-21 17:38:34 +0200

    added query for participants of event

--- core/src/main/java/org/diningdevelopers/core/database/dao/UserDao.java
+++ core/src/main/java/org/diningdevelopers/core/database/dao/UserDao.java
@@ -3,14 +3,21 @@ package org.diningdevelopers.core.database.dao;
 import java.util.List;
 
 import javax.inject.Named;
+import javax.mail.Address;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
 
 import org.diningdevelopers.core.database.dao.helper.CriteriaHelper;
 import org.diningdevelopers.core.database.dao.helper.JpaUtils;
 import org.diningdevelopers.core.database.entities.EventEntity;
 import org.diningdevelopers.core.database.entities.UserEntity;
 import org.diningdevelopers.core.database.entities.UserEntity_;
+import org.diningdevelopers.core.database.entities.VoteEntity;
+import org.diningdevelopers.core.database.entities.VoteEntity_;
 
 @Named
 public class UserDao {
@@ -27,20 +34,30 @@ public class UserDao {
 	}
 
 	public UserEntity findByUsername(String username) {
-		CriteriaHelper<UserEntity> helper = new CriteriaHelper<>(entityManager, UserEntity.class);
+		CriteriaHelper<UserEntity> helper = new CriteriaHelper<>(entityManager,
+				UserEntity.class);
 
 		helper.addEqual(UserEntity_.username, username);
 
 		return helper.getSingleResultOrNull();
-	}	
+	}
 
 	public void persist(UserEntity developer) {
 		entityManager.persist(developer);
 	}
 
-	public List<UserEntity> findUsersOfEvent(EventEntity map) {
-		// TODO find Participants of Event
-		return null;
+	public List<UserEntity> findUsersOfEvent(EventEntity event) {
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<UserEntity> query = builder.createQuery(UserEntity.class);
+		Root<UserEntity> users = query.from(UserEntity.class);
+		Root<VoteEntity> votes = query.from(VoteEntity.class);
+		query.select(users)
+				.distinct(true)
+				.where(builder.and(
+						builder.equal(users, votes.get(VoteEntity_.developer)),
+						builder.equal(votes.get(VoteEntity_.event), event)));
+
+		return entityManager.createQuery(query).getResultList();
 	}
 
 }
