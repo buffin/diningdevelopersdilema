8c8b3242dc12d94fd907a0faf8d1fa597efbf2bd
Nils Eckert <nils.eckert@eckert-partner.it>
2013-05-03 18:57:13 +0200

    Closes #26: Do not use EJB for Shiro JndiRealm

--- src/main/java/org/diningdevelopers/security/ShiroSecurityRealm.java
+++ src/main/java/org/diningdevelopers/security/ShiroSecurityRealm.java
@@ -1,9 +1,5 @@
 package org.diningdevelopers.security;
 
-import javax.ejb.EJB;
-import javax.ejb.Local;
-import javax.ejb.Stateless;
-
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -16,18 +12,13 @@ import org.apache.shiro.authz.Authorizer;
 import org.apache.shiro.authz.permission.PermissionResolverAware;
 import org.apache.shiro.authz.permission.RolePermissionResolverAware;
 import org.apache.shiro.realm.AuthorizingRealm;
-import org.apache.shiro.realm.Realm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.Initializable;
 import org.diningdevelopers.model.UserModel;
 import org.diningdevelopers.service.UserService;
+import org.diningdevelopers.utils.InitialContextUtils;
 
-@Stateless
-@Local(Realm.class)
 public class ShiroSecurityRealm extends AuthorizingRealm implements Authorizer, Initializable, PermissionResolverAware, RolePermissionResolverAware {
-
-	@EJB
-	private UserService developerService;
 
 	public ShiroSecurityRealm() {
 		CredentialsMatcher credentialsMatcher = new HashedCredentialsMatcher("SHA-1");
@@ -39,7 +30,9 @@ public class ShiroSecurityRealm extends AuthorizingRealm implements Authorizer, 
 		UsernamePasswordToken upToken = (UsernamePasswordToken) token;
 		String username = upToken.getUsername();
 
-		UserModel user = developerService.findByUsername(username);
+		UserService userService = InitialContextUtils.doLookup("java:module/UserService");
+
+		UserModel user = userService.findByUsername(username);
 
 		if (user == null) {
 			throw new AuthenticationException();
---
+++ src/main/java/org/diningdevelopers/utils/InitialContextUtils.java
@@ -0,0 +1,20 @@
+package org.diningdevelopers.utils;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class InitialContextUtils {
+
+	/**
+	 * 
+	 * @param name
+	 * @return handles NamingException and returns null 
+	 */
+	public static <T> T doLookup(String name) {
+		try {
+			return InitialContext.doLookup(name);
+		} catch (NamingException e) {
+			return null;
+		}
+	}
+}
